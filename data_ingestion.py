d48ff6be682a6a7bab30664b1a334ada93e09b5c
Khadazhaia Grayson <khadazhaia.grayson@outlook.com>
2025-01-30 20:11:55 -0500

    cleaning advanced data

--- data_ingestion.py
+++ data_ingestion.py
@@ -1,17 +1,34 @@
 import pandas as pd
 import glob
 
-# Get a list of all CSV files in the specified directory
-file_list = glob.glob("data/player_stats/*.csv")
+# Get a list of all CSV files in the player stats
+player_list = glob.glob("data/player_stats/*.csv")
 
 # Create an empty dataframe to store the merged data
 merged_df = pd.DataFrame()
 
 # Loop through each file, read it into a dataframe, and append it to the merged dataframe
-for file in file_list:
+for file in player_list:
     df = pd.read_csv(file)
     merged_df = pd.concat([merged_df, df], ignore_index=True)
 
 merged_df.set_index("Year", inplace = True)
     
 merged_df.to_csv("data/player_stats/merged2000-2024.csv")
+
+
+advanced_list = glob.glob("data/advanced_stats/*.csv")
+
+print(advanced_list)
+
+# merged_df2 = pd.DataFrame()
+
+# for files in advanced_list:
+#     df2 = pd.read_csv(files)
+#     merged_df2 = pd.concat([merged_df2, df2], ignore_index=True)
+
+# print(merged_df2)
+
+# merged_df2.set_index("Year", inplace = True)
+    
+# merged_df2.to_csv("data/advanced_stats/merged2000-2024.csv")
