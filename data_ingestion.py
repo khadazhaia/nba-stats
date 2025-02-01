440fa12206ad99db3e7339f5906ad2957d23c643
Khadazhaia Grayson <khadazhaia.grayson@outlook.com>
2025-02-01 13:10:56 -0500

    merge all datasets

--- data_ingestion.py
+++ data_ingestion.py
@@ -1,34 +1,28 @@
 import pandas as pd
 import glob
 
-# Get a list of all CSV files in the player stats
-player_list = glob.glob("data/player_stats/*.csv")
-
-# Create an empty dataframe to store the merged data
-merged_df = pd.DataFrame()
-
-# Loop through each file, read it into a dataframe, and append it to the merged dataframe
-for file in player_list:
-    df = pd.read_csv(file)
-    merged_df = pd.concat([merged_df, df], ignore_index=True)
-
-merged_df.set_index("Year", inplace = True)
+def merge_csv_files(path, output_file):
+    """
+    Merges all CSV files from the given path into one DataFrame and saves it to a new CSV file.
     
-merged_df.to_csv("data/player_stats/merged2000-2024.csv")
-
-
-advanced_list = glob.glob("data/advanced_stats/*.csv")
-
-print(advanced_list)
-
-# merged_df2 = pd.DataFrame()
-
-# for files in advanced_list:
-#     df2 = pd.read_csv(files)
-#     merged_df2 = pd.concat([merged_df2, df2], ignore_index=True)
-
-# print(merged_df2)
-
-# merged_df2.set_index("Year", inplace = True)
+    Parameters:
+    - path: str, path to the directory containing the CSV files.
+    - output_file: str, outputs the merged CSV file.
+    """
+    # Get a list of all CSV files in the path
+    file_list = glob.glob(f"{path}/*.csv")
+ 
+    # Loop through each file, read it into a dataframe, and append it 
+    dfs = []
+    for file in file_list:
+            df = pd.read_csv(file)
+            dfs.append(df)
     
-# merged_df2.to_csv("data/advanced_stats/merged2000-2024.csv")
+    # Merge the dataframes, set index as year, and save it 
+    merge_df = pd.concat(dfs, ignore_index=True)
+    merge_df.set_index("Year", inplace=True)      
+    merge_df.to_csv(output_file)
+
+merge_csv_files("data/player_stats", "data/player_stats/merge2000-2024.csv")
+merge_csv_files("data/advanced_stats", "data/advanced_stats/merge2000-2024.csv")
+merge_csv_files("data/team_stats", "data/team_stats/merge2000-2024.csv")
